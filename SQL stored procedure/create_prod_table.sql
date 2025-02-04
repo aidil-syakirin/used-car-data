da25374992da8d60ef976d3b5d3360cb3df05236
aidil-syakirin <106723859+aidil-syakirin@users.noreply.github.com>
2025-02-04 06:26:15 -0800

    Create create_prod_table.sql
    creating prod table

---
+++ SQL stored procedure/create_prod_table.sql
@@ -0,0 +1,14 @@
+create table dbo.prod_table (
+listing_id varchar(255), 
+dim_car_model varchar(20),
+fct_price numeric(18,0),
+fct_installment numeric(18,0),
+fct_year int,
+fct_mileage numeric(18,0),
+dim_color varchar(20),
+dim_location varchar(80),
+dim_state varchar(80),
+url varchar(80),
+image text,
+dim_processing_date date
+)
